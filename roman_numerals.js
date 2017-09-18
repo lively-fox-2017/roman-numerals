34fc59f9fa1c178b281cf82003da5ba0fccc3cb9
ameliarahman <amel.rahman5@gmail.com>
2017-09-18 16:15:41 +0700

    Revisi

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,6 +1,6 @@
 function to_roman (num) {
-  var angka = [1,4,5,9,10,40, 50,90, 100, 500, 1000];
-  var roman = ['I','IV','V','IX','X','XL','L','XC','C','D','M'];
+  var angka = [1,4,5,9,10,40, 50,90, 100, 400, 500, 1000];
+  var roman = ['I','IV','V','IX','X','XL','L','XC','C','CD','D','M'];
   var angkaRoman = '';
   for(var i= angka.length-1; i>=0; i--){
     while(num >= angka[i]){
