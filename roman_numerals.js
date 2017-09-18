5537e50040a9ce5c8de7b96f5c1d186d6c38ce3b
azhariemuhammad <azharie_muhammad@live.com>
2017-09-18 15:30:40 +0700

    selesai roman

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,18 @@
-function to_roman (num) {
+function to_roman(num) {
   // your implementation code here
+  let result = "";
+  // yang pertama kita buat arr
+  let angka = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  let roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+
+  for (let i = 0; i < angka.length; i++) { // looping untuk akses array angkanya
+    while (num % angka[i] < num) { // modulus num denga arrAngka yang mendekati dengan num
+      result += roman[i]; // jika dapat maka di push ke result
+      num -= angka[i]; // substract angka[i];
+    }
+  }
+  return result;
+
 }
 
 // Drive code
