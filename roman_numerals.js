37190d4dc90f45ce4314cfe69a86dc9d961b7261
Bambang Kurniawan <bembkurniawan@gmail.com>
2017-09-18 15:15:12 +0700

    complete to_roman() function

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,6 +1,43 @@
 function to_roman (num) {
-  // your implementation code here
+	let result = '';
+	const arabicToRoman = {
+		'M': 1000,
+		'CM': 900,
+		'D': 500,
+		'CD': 400,
+		'C': 100,
+		'XC': 90,
+		'L': 50,
+		'XL': 40,
+		'X': 10,
+		'IX': 9,
+		'V': 5,
+		'IV': 4,
+		'I': 1,
+	}
+
+	for (let roman in arabicToRoman) {
+		let arabic = arabicToRoman[roman]
+		while (num >= arabic) {
+			num -= arabic;
+			result += roman;
+		}
+	}
+
+	return result;
 }
+	// [
+	// 	[1, 'I'],
+	// 	[4, 'IV'],
+	// 	[5, 'V'],
+	// 	[9, 'IX'],
+	// 	[40, 'XL'],
+	// 	[90, 'XC'],
+	// 	[100, 'C'],
+	// 	[400, 'CD'],
+	// 	[900, 'CM'],
+	// 	[1000, 'M']
+	// ];
 
 // Drive code
 console.log('My totally sweet testing script for new roman\n')
