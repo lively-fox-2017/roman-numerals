6ca11f5815a95b64b6a8aea7c96d59ee85c30445
jasoneffendi <jasoneffendi27@gmail.com>
2017-09-18 14:31:28 +0700

    Roman Numerals Done

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,6 +1,18 @@
 function to_roman (num) {
-  // your implementation code here
-}
+  // your implementation code herefunction romanize(num) {
+    var lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1};
+    // var rom = ['M', ' CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+    // var number = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+
+    var roman = '';
+    for ( i in lookup) {
+      while ( num >= lookup[i] ) {
+        roman += i;
+        num -= lookup[i];
+      }
+    }
+    return roman;
+  }
 
 // Drive code
 console.log('My totally sweet testing script for new roman\n')
