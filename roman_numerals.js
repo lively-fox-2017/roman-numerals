3d2ca42e1c0fe334ce534f7824a9198479f75500
harynp <harynugrahaputra@gmail.com>
2017-09-18 17:16:20 +0700

    Roman Numerals Selesai

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,16 @@
-function to_roman (num) {
-  // your implementation code here
+function to_roman(num) {
+
+var temp = '';
+angka = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+romawi = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+var hasil = '';
+for (var i = 0; i < angka.length; i++) {
+  while (num >= angka[i]) {
+    hasil += romawi[i];
+    num -= angka[i]
+  }
+}
+return hasil;
 }
 
 // Drive code
