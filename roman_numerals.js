a8d91b78531dc114bfce350f7e099eba6464de5a
Rahmat Hidayat <email": null>
2017-09-18 16:30:41 +0700

    roman numeral release 0

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,34 @@
 function to_roman (num) {
   // your implementation code here
+  let romanObj = {
+    'M'   : 1000,
+    'CM'  : 900,
+    'D'   : 500,
+    'CD'  : 400,
+    'C'   : 100,
+    'XC'  : 90,
+    'L'   : 50,
+    'XL'  : 40,
+    'X'   : 10,
+    'IX'  : 9,
+    'V'   : 5,
+    'IV'  : 4,
+    'I'   : 1,
+  };
+
+  var romawi = "";
+
+  for(i in romanObj){
+    // console.log(i);
+    while( num >= romanObj[i]){
+      romawi += i;
+      num -= romanObj[i];
+
+    }
+
+
+  }
+  return romawi
 }
 
 // Drive code
