9cd31761f80fe578b396963a6e237e564f46f418
pjap <pranajaya.pratama@gmail.com>
2017-09-26 09:26:28 +0700

    ROMAN NUMERALS

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,7 +1,35 @@
 function to_roman (num) {
-  // your implementation code here
+	var result = '';
+
+  var decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var roman = ["M", "CM","D","CD","C", "XC", "L", "XL", "X","IX","V","IV","I"];
+  for (var i = 0;i<=decimal.length;i++) {
+  // melooping ke array yang kita buat
+    while (num >= decimal[i]) {
+    // terus mencari hingga sisa bagi kurang dari num
+      result += roman[i];
+      // masukan array roman sesuai index ke variabel result
+      num -= decimal[i];
+      // kurangi num dengan nilai decimal sesuai index , lalu ulangi ang
+    }
+  }
+  return result;
+
+  // var hasil = '';
+  // var arabic = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 3 , 2 , 1];
+  // var roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV','III', 'II' , 'I'];
+
+  // for (var i = 0; i < arabic.length; i++){
+  // 	if (num >= arabic[i]){
+  // 		hasil = hasil + roman[i];
+  // 		num = num - arabic[i];
+  // 	}
+  // }
+  // return hasil;
+
 }
 
+//console.log(to_roman(1646))
 // Drive code
 console.log('My totally sweet testing script for new roman\n')
 console.log('input | expected | actual')
